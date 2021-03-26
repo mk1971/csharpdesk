ee9f3b414c3d8e183f0e2222d8260af9c20754c1
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:23:25 -0300

    file stream

---
+++ FileStreamClass/FileStreamClass/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileStreamClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.FileStream wFile;
+                byte[] byteData = null;
+                byteData = Encoding.ASCII.GetBytes("FileStream Test");
+                wFile = new FileStream("c:\\streamtest.txt", FileMode.Append);
+                wFile.Write(byteData, 0, byteData.Length);
+                wFile.Close();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
