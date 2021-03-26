5c581a138f4af9ca8b53a7a26161e9c6dfd54c96
mk1971 <wagner.maki@hotmail.com>
2021-03-26 16:58:45 -0300

    multiple class

---
+++ ConsoleMultipleClass/ConsoleMultipleClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleMultipleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.NeonNumber();
+            Console.ReadLine();
+        }
+        void NeonNumber()
+        {
+            Console.WriteLine("Enter your number to check number is neon or not");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int temp = input * input;
+
+            string tempString = Convert.ToString(temp);
+
+            char[] charArray = tempString.ToCharArray();
+
+
+            int sum = 0;
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                string sumTemp = Convert.ToString(charArray[i]);
+                sum += Convert.ToInt32(sumTemp);
+            }
+
+            if (sum == input)
+            {
+                Console.WriteLine("Number is neon");
+            }
+            else
+            {
+                Console.WriteLine("Number is not neon");
+            }
+        }
+    }
+}
