d25516800fb9671487d70a7de7572d79221d76fd
mk1971 <wagner.maki@hotmail.com>
2021-03-26 13:01:40 -0300

    TreeView Control

---
+++ TreeViewControl/TreeViewControl/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TreeNode tNode;
+            tNode = treeView1.Nodes.Add("Websites");
+
+            treeView1.Nodes[0].Nodes.Add("Net-informations.com");
+            treeView1.Nodes[0].Nodes[0].Nodes.Add("CLR");
+
+            treeView1.Nodes[0].Nodes.Add("Vb.net-informations.com");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("String Tutorial");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("Excel Tutorial");
+
+            treeView1.Nodes[0].Nodes.Add("Csharp.net-informations.com");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("ADO.NET");
+            treeView1.Nodes[0].Nodes[2].Nodes[0].Nodes.Add("Dataset");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(treeView1.SelectedNode.FullPath.ToString());
+        }
+    }
+}
+;
