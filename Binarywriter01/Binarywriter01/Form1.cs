a43c6b641cc8701e9bbf58917b6beaa89f7230ae
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:38:59 -0300

    binary writer

---
+++ Binarywriter01/Binarywriter01/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Binarywriter01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream writeStream;
+            try
+            {
+                writeStream = new FileStream("c:\\csharp.net-informations.dat", FileMode.Create);
+                BinaryWriter writeBinay = new BinaryWriter(writeStream);
+                writeBinay.Write("CSharp.net-informations.com binary writer test");
+                writeBinay.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
