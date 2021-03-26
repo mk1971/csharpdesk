18e76633a4feeb0c1e79b31d74b9f32633cd8ed3
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:36:32 -0300

    textreader

---
+++ Textwriter/Textwriter/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Textwriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.TextWriter writeFile = new StreamWriter("c:\\textwriter.txt");
+                writeFile.WriteLine("csharp.net-informations.com");
+                writeFile.Flush();
+                writeFile.Close();
+                writeFile = null;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
