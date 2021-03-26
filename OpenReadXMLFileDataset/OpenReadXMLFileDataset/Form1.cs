066ff819dbf61dce538ad553ca71a9474729c6b5
mk1971 <wagner.maki@hotmail.com>
2021-03-26 15:16:23 -0300

    open and read xml file from dataset

---
+++ OpenReadXMLFileDataset/OpenReadXMLFileDataset/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace OpenReadXMLFileDataset
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create("Product.xml", new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlFile);
+            int i = 0;
+            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                MessageBox.Show(ds.Tables[0].Rows[i].ItemArray[2].ToString());
+            }
+        }
+    }
+}
