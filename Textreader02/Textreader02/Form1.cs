16d8520cb055c3880af46d93a0dd3034b39f4b90
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:28:55 -0300

    textreader modo simples

---
+++ Textreader02/Textreader02/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Textreader02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string line = null;
+                System.IO.TextReader readFile = new StreamReader("C:\\net-informations.txt");
+                line = readFile.ReadToEnd();
+                MessageBox.Show(line);
+                readFile.Close();
+                readFile = null;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
