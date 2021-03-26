d3d2b4db9ef1520362d5524500ee5bf82b82dfb0
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:25:27 -0300

    text reader

---
+++ TextreaderClass/TextreaderClass/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextreaderClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string line = null;
+                System.IO.TextReader readFile = new StreamReader("C:\\csharp.net-informations.txt");
+                while (true)
+                {
+                    line = readFile.ReadLine();
+                    if (line != null)
+                    {
+                        MessageBox.Show(line);
+                    }
+                }
+                readFile.Close();
+                readFile = null;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
