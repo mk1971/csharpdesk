d4b3b2b202acef9d8622fd891274ff992e5d3998
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:13:10 -0300

    File class

---
+++ FileClass/FileClass/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("C:\\testFile.txt"))
+            {
+                //shows message if testFile exist
+                MessageBox.Show("File 'testFile' Exist ");
+            }
+            else
+            {
+                //create the file testFile.txt
+                File.Create("C:\\testFile.txt");
+                MessageBox.Show("File 'testFile' created ");
+            }
+        }
+    }
+}
