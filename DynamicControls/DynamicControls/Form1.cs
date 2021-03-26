ec7c255a6722d896a231dced96f07f1c5d60b62a
mk1971 <wagner.maki@hotmail.com>
2021-03-26 13:07:02 -0300

    Dynamic Controls

---
+++ DynamicControls/DynamicControls/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicControls
+{
+    public partial class Form1 : Form
+    {
+        int cLeft = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddNewTextBox();
+        }
+
+        public System.Windows.Forms.TextBox AddNewTextBox()
+        {
+            System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox();
+            this.Controls.Add(txt);
+            txt.Top = cLeft * 25;
+            txt.Left = 100;
+            txt.Text = "TextBox" + this.cLeft.ToString();
+            cLeft = cLeft + 1;
+            return txt;
+        }
+    }
+}
