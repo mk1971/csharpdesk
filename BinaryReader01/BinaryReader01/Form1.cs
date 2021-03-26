4f040fc7e163fa4cf1b84e3c5a02be2fd7169356
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:43:12 -0300

    BinaryReader

---
+++ BinaryReader01/BinaryReader01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BinaryReader01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream readStream;
+            string msg = null;
+            try
+            {
+                readStream = new FileStream("c:\\csharp.net-informations.dat", FileMode.Open);
+                BinaryReader readBinary = new BinaryReader(readStream);
+                msg = readBinary.ReadString();
+                MessageBox.Show(msg);
+                readStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
