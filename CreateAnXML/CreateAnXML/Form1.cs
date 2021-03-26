127c869bc58eb077968f3580c8c0635ea32b7b8b
mk1971 <wagner.maki@hotmail.com>
2021-03-26 14:59:16 -0300

    create an xml

---
+++ CreateAnXML/CreateAnXML/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CreateAnXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlTextWriter writer = new XmlTextWriter("product.xml", System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("Table");
+            createNode("1", "Product 1", "1000", writer);
+            createNode("2", "Product 2", "2000", writer);
+            createNode("3", "Product 3", "3000", writer);
+            createNode("4", "Product 4", "4000", writer);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+            MessageBox.Show("XML File created ! ");
+        }
+
+        private void createNode(string pID, string pName, string pPrice, XmlTextWriter writer)
+        {
+            writer.WriteStartElement("Product");
+            writer.WriteStartElement("Product_id");
+            writer.WriteString(pID);
+            writer.WriteEndElement();
+            writer.WriteStartElement("Product_name");
+            writer.WriteString(pName);
+            writer.WriteEndElement();
+            writer.WriteStartElement("Product_price");
+            writer.WriteString(pPrice);
+            writer.WriteEndElement();
+            writer.WriteEndElement();
+        }
+    }
+}
