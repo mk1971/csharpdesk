b016561c8b3dc2c4af2c5bdfdf1d89e1c3c34335
mk1971 <wagner.maki@hotmail.com>
2021-03-28 13:11:20 -0300

    dicionario corrente

---
+++ CurrentDictionaryInCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CurrentDictionaryInCSharp
+{
+    public class Program
+    {
+        static Dictionary<string, int> _mydic = new Dictionary<string, int>();
+        static ConcurrentDictionary<string, int> _mydictConcu = new ConcurrentDictionary<string, int>();
+        static void Main(string[] args)
+        {
+            Thread mythread1 = new Thread(new ThreadStart(InsertData));
+            Thread mythread2 = new Thread(new ThreadStart(InsertData));
+            mythread1.Start();
+            mythread2.Start();
+            mythread1.Join();
+            mythread2.Join();
+
+            Thread mythread11 = new Thread(new ThreadStart(InsertDataConcu));
+            Thread mythread21 = new Thread(new ThreadStart(InsertDataConcu));
+            mythread11.Start();
+            mythread21.Start();
+            mythread11.Join();
+            mythread21.Join();
+
+            Console.WriteLine($"Result in Dictionary : {_mydic.Values.Count}");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine($"Result in Concurrent Dictinary : {_mydictConcu.Values.Count}");
+            Console.ReadKey();
+        }
+
+        static void InsertDataConcu()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _mydictConcu.TryAdd(Guid.NewGuid().ToString(), i);
+            }
+        }
+
+        static void InsertData()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _mydic.Add(Guid.NewGuid().ToString(), i);
+            }
+        }
+    }
+}
