8616d5174affc3a0ee7086acbae4fc316bf1b0a0
mk1971 <wagner.maki@hotmail.com>
2021-03-30 10:05:46 -0300

    prefix e posfix exemplo 1

---
+++ PrefixPostfix_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrefixPostfix_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 0, var2 = 0;
+            Console.WriteLine("Prefix on return: " + PlusOnePrefix(var1));
+            Console.WriteLine("Postfix on return: " + PlusOnePostfix(var2));
+        }
+        public static int PlusOnePrefix(int x)
+        {
+            return ++x; // => increments first, and return the result
+        }
+        public static int PlusOnePostfix(int x)
+        {
+            return x++; // => return the value x and then increments
+        }
+    }
+}
