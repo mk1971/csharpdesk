cd8ad89dbe5d3e7fbfbb9577d1b2daa795ced5c3
mk1971 <wagner.maki@hotmail.com>
2021-03-30 10:07:19 -0300

    prefix posfix exemplo 2

---
+++ PrefixPostfix_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrefixPostfix_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (++x < 3)
+            {
+                Console.WriteLine("Using Prefix, x value is: " + x);
+            }
+            x = 0;
+            while (x++ < 3)
+            {
+                Console.WriteLine("Using Postfix, x value is: " + x);
+            }
+        }
+    }
+}
