c1e1dabc6844093c34c9ca5b5600dfc1da54e8c9
mk1971 <wagner.maki@hotmail.com>
2021-03-30 09:55:59 -0300

    Exception simples

---
+++ ExceptionsSimples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExceptionsSimples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int a = 2, b = 0;
+                float z = a / b;
+                Console.WriteLine("His will not executed");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("This will always be executed");
+            }
+        }
+    }
+}
