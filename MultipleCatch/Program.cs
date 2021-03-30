4d047353b749f5b217b23876d28ba6e0f776a719
mk1971 <wagner.maki@hotmail.com>
2021-03-30 09:58:00 -0300

    multiple catch

---
+++ MultipleCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MultipleCatch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CatchException sampleException = new CatchException();
+            sampleException.BeforeCSharp6("FormatException");
+            sampleException.BeforeCSharp6("OverflowException");
+            sampleException.BeforeCSharp6("Exception");
+        }
+    }
+    class CatchException
+    {
+        public void BeforeCSharp6(string ExceptionType)
+        {
+            try
+            {
+                switch (ExceptionType)
+                {
+                    case "FormatException":
+                        throw new FormatException();
+                    case "OverflowException":
+                        throw new OverflowException();
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException || ex is OverflowException)
+                {
+                    Console.WriteLine("This is a FormatException or OverflowException ");
+                    return;
+                }
+                Console.WriteLine("This is NOT a FormatException or OverflowException ");
+            }
+            return;
+        }
+    }
+}
